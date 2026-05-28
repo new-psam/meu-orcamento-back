31adb1a0aa7fe7f37c693dc8d56885c0ee01b9f8
new-psam <marcelino.patricio@hotmail.com>
2026-05-27 21:03:44 -0300

    docs: adiciona documentacao interativa com swagger e openapi

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import "dotenv/config";
 import  express  from "express"
 import { transactionRoutes } from "./routes/transaction.routes";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger";
 
 const app = express();
 
 app.use(express.json());
+
+// Rota oficial da nossa Documentação (A Vitrine!)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Nossas rotas de negócio
 app.use("/transactions", transactionRoutes);
 
 export { app };
---
+++ src/config/swagger.ts
@@ -0,0 +1,22 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API - Meu Orçamento",
+            version: "1.0.0",
+            description: "Documentação oficial da API de controle financeiro pessoal.",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+                description: "Servidor Local",
+            },
+        ],
+    },
+    // Aqui indicamos onde o Swagger deve procurar as anotações para gerar a vitrine
+    apis: ["./src/routes/*.ts", "./src/docs/*.yaml"],
+};
+
+export const swaggerSpec = swaggerJSDoc(options);
