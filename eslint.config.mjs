9bb0340a8c260ce819d3d3d47df4fa1fddf0f1a9
new-psam <marcelino.patricio@hotmail.com>
2026-08-06 08:33:32 -0300

    chore: atualiza configuração do eslint com checagem de tipos strita

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    
+    {
+        files: ["src/**/*.ts"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
+
+            parserOptions: {
+                project: "./tsconfig.json",
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+
+    ...tseslint.configs.recommendedTypeChecked,
+
+    {
+        rules: {
+            "no-undef": "off", // O typeScript já faz o trabalho de verificar variáveis indefinidas
+
+            // Boas práticas
+            "@typescript-eslint/consistent-type-imports": "error",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_",
+                },
+            ],
+
+            "@typescript-eslint/no-floating-promises": "error",
+            "@typescript-eslint/no-misused-promises": "error",
+            "@typescript-eslint/await-thenable": "error",
+
+            // Permite async sem await (útil em controllers)
+            "@typescript-eslint/require-await": "off",
+        },
+    }
+);
+        
